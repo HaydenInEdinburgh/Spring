113698c9378abab11b4aa01d0335be51c46c2a3c
Pengyi Ye <pye3@wpi.edu>
2020-09-29 18:49:47 -0400

    Updated on 29/09/2020

---
+++ char04-di-anno/ReadMe.txt
@@ -0,0 +1,15 @@
+Char04-di-anno:
+
+1. Motivation:
+    Creating the java objects and editing their args by the annotation of
+    spring instead of xml file.
+
+2.  Steps:
+    a.  insert dependencies.
+    b.  create class, insert anno into class
+    c.  create configuration file.
+        announce the tags, name the position of the annos in your project.
+    d.  use anno to create object, create container.
+
+3.  Annotations
+    a.  @
---
+++ char04-di-anno/src/test/java/com/bernieye/myTest01.java
@@ -0,0 +1,17 @@
+package com.bernieye;
+
+import com.bernieye.ba01.Student;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class myTest01 {
+
+    @Test
+    public void test01(){
+        String config = "applicationContext.xml";
+        ApplicationContext ac = new ClassPathXmlApplicationContext(config);
+        Student student = (Student) ac.getBean("myStudent");
+        System.out.println("student is "+ student);
+    }
+}
